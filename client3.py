edd8e9bc71788d2b124587082625e87087ae1298
Tikshil umre <161268144+tiksh09@users.noreply.github.com>
2024-07-29 17:54:29 +0530

    Update client3.py

--- client3.py
+++ client3.py
@@ -18,6 +18,7 @@
 #  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 #  DEALINGS IN THE SOFTWARE.
 
+
 import json
 import random
 import urllib.request
@@ -28,32 +29,37 @@ QUERY = "http://localhost:8080/query?id={}"
 # 500 server request
 N = 500
 
-
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price) / 2
     return stock, bid_price, ask_price, price
-
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
-
+    if price_b == 0:
+        return None  # or you could return float('inf') if you prefer
+    return price_a / price_b
 
 # Main
 if __name__ == "__main__":
-    # Query the price once every N seconds.
-    for _ in iter(range(N)):
+    for _ in range(N):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-        """ ----------- Update to get the ratio --------------- """
+        prices = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+            prices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+        # Assuming you want to get the ratio of two specific stocks, e.g., 'ABC' and 'DEF'
+        stock_a = 'ABC'
+        stock_b = 'DEF'
+        if stock_a in prices and stock_b in prices:
+            ratio = getRatio(prices[stock_a], prices[stock_b])
+            print("Ratio of %s to %s: %s" % (stock_a, stock_b, ratio))
+        else:
+            print("Required stocks are not available in the fetched data.")
+
